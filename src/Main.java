ec3e5eff402d1f7463b45bc41a50a2f3cc9102f6
Michael <Shires>
2023-11-19 00:18:52 -0500

    Got the server communicating, and made the DBManager class and was able to connect it to a MySQL database. Learned the syntax for the db op's we want to use.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 import java.security.NoSuchAlgorithmException;
 
+
 public class Main {
     //This is a comment to test git features.
     public Main() {
     }
     public static void main(String[] args) {
+        
         System.out.println("Hello world!");
-        Hasher hasher = new Hasher("UraniumArmadillo");
+        //Server server = new Server(6969);
+        //Client client = new Client("0.0.0.0", 6969);
+
+        //        Hasher hasher = new Hasher("UraniumArmadillo");
     }
 
 }
